37c1cc7ba262400d36e7e85ba9eb3ea154d3b3ce
chaiwatkang007 <chaiwat_noon007@hotmail.com>
2024-05-21 23:26:36 +0700

    update route medicine

--- server/route/medicine.route.tsx
+++ server/route/medicine.route.tsx
@@ -21,6 +21,14 @@ medicineRoute
         res.status(400).json({ message: err?.message });
     }
   })
+  .post("/balance", async (req: Request, res: Response) => {
+    try {
+        const result = await MedicineController.balance(req?.body);
+        res.status(200).json({ result });
+    } catch (err: any) {
+        res.status(400).json({ message: err?.message });
+    }
+  })
 
 
 export default medicineRoute;
